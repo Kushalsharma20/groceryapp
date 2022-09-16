e9fbc5d484cb82d8da45c1467da3c8b8bbf32a25
Kushalsharma20 <kushalsharma5520@gmail.com>
2022-09-16 19:11:36 +0300

    worked on home screen

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import "package:flutter/material.dart";
 
 class HomeScreen extends StatelessWidget {
@@ -8,110 +6,125 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffcbcbcb),
-      drawer: Drawer(),
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.black),
-        actions: [
-          CircleAvatar(
-              radius: 12,
-              backgroundColor: Color.fromARGB(167, 31, 167, 230),
-              child: Icon(
-                Icons.search,
-                size: 18,
-                color: Colors.black,
-              )),
-          Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 5),
-              child: CircleAvatar(
+        backgroundColor: Color(0xffcbcbcb),
+        drawer: Drawer(),
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.black),
+          actions: [
+            CircleAvatar(
                 radius: 12,
-                backgroundColor: Color.fromARGB(255, 56, 214, 64),
+                backgroundColor: Color.fromARGB(167, 31, 167, 230),
                 child: Icon(
-                  Icons.shop,
+                  Icons.search,
                   size: 18,
                   color: Colors.black,
-                ),
-              )),
-        ],
-        title: Text(
-          'Home',
-          style: TextStyle(color: Colors.black, fontSize: 17),
-        ),
-        backgroundColor: Color(0xffd6b738),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-        child: Column(
-          children: [
-            Container(
-              height: 150,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: NetworkImage(
-                          'https://cdn-2.tstatic.net/travel/foto/bank/images/blueberry_20160901_120145.jpg')),
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(10)),
-            ),
+                )),
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Herbs Seasonings'),
-                  Text(
-                    'View All',
-                    style: TextStyle(color: Colors.grey),
-                  )
-                ],
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                child: CircleAvatar(
+                  radius: 12,
+                  backgroundColor: Color.fromARGB(255, 56, 214, 64),
+                  child: Icon(
+                    Icons.shop,
+                    size: 18,
+                    color: Colors.black,
+                  ),
+                )),
+          ],
+          title: Text(
+            'Home',
+            style: TextStyle(color: Colors.black, fontSize: 17),
+          ),
+          backgroundColor: Color(0xffd6b738),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            child: Column(children: [
+              Container(
+                height: 150,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            'https://cdn-2.tstatic.net/travel/foto/bank/images/blueberry_20160901_120145.jpg')),
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(10)),
               ),
-            ),
-            Row(
-              children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Herbs Seasonings'),
+                    Text(
+                      'View All',
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                ),
+              ),
+              Row(children: [
                 Container(
-                  width: 160,
-                  height: 230,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
+                    width: 160,
+                    height: 230,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Column(children: [
                       Expanded(
-                          flex: 2,
                           child: Image.network(
                               "https://www.veggycation.com.au/siteassets/veggycationvegetable/basil.jpg")),
                       Expanded(
-                          child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 10, vertical: 5),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Fresh Basil",
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 5),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Fresh Basil",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                            ),
-                            Text(
-                              "1000/=/100g",
-                              style: TextStyle(
-                                color: Colors.grey,
+                              Text(
+                                "1000/=/100g",
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                ),
                               ),
-                            ),
-                          ],
+                              Row(
+                                children: [
+                                  Expanded(
+                                      child: Container(
+                                    height: 30,
+                                    width: 50,
+                                    decoration: BoxDecoration(
+                                        color: Colors.black,
+                                        borderRadius:
+                                            BorderRadius.circular(10)),
+                                  )),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Expanded(
+                                    child: Container(
+                                        height: 30,
+                                        width: 50,
+                                        decoration: BoxDecoration(
+                                            color: Colors.red,
+                                            borderRadius:
+                                                BorderRadius.circular(5))),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
-                      ))
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
+                      )
+                    ]))
+              ])
+            ])));
   }
 }

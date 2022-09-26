b9a7749e8ac5384175409ea444f3d643325cf391
Kushalsharma20 <kushalsharma5520@gmail.com>
2022-09-26 18:46:33 +0300

    added

--- lib/screens/my_profile/my_profile.dart
+++ lib/screens/my_profile/my_profile.dart
@@ -6,6 +6,21 @@ class MyProfile extends StatelessWidget {
   const MyProfile({super.key});
 
   @override
+  Widget listTile({IconData? icon, required String title}) {
+    return Column(
+      children: [
+        Divider(
+          height: 1,
+        ),
+        ListTile(
+          leading: Icon(icon),
+          title: Text(title),
+          trailing: Icon(Icons.arrow_forward_ios),
+        )
+      ],
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: primarycolor,
@@ -32,9 +47,68 @@ class MyProfile extends StatelessWidget {
                   decoration: BoxDecoration(
                       color: scaffoldBackgroundColor,
                       borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30),
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15),
                       )),
+                  child: ListView(children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Container(
+                          width: 250,
+                          height: 80,
+                          padding: EdgeInsets.only(left: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Kushal",
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                        color: textColor),
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text("kushal20.ks@gmail.com"),
+                                ],
+                              ),
+                              CircleAvatar(
+                                radius: 15,
+                                backgroundColor: primarycolor,
+                                child: CircleAvatar(
+                                  radius: 12,
+                                  child: Icon(
+                                    Icons.edit,
+                                    color: primarycolor,
+                                  ),
+                                  backgroundColor: scaffoldBackgroundColor,
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                    listTile(
+                        icon: Icons.location_on_outlined,
+                        title: "My Delivery Address"),
+                    listTile(
+                        icon: Icons.person_outlined, title: "Refer To Friends"),
+                    listTile(
+                        icon: Icons.file_copy_outlined,
+                        title: "Terms & Conditions"),
+                    listTile(
+                        icon: Icons.policy_outlined, title: "privacy Policy"),
+                    listTile(icon: Icons.add_chart, title: "About"),
+                    listTile(
+                        icon: Icons.exit_to_app_outlined, title: "Log Out"),
+                  ]),
                 )
               ],
             ),

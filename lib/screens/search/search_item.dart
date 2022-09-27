5be68f1d46c3c4203120d6c36cf284fdbb2a9091
Kushalsharma20 <kushalsharma5520@gmail.com>
2022-09-27 22:19:25 +0300

    more work

--- lib/screens/search/search_item.dart
+++ lib/screens/search/search_item.dart
@@ -6,43 +6,103 @@ class SearchItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-            child: Container(
-          height: 100,
-          child: Center(
-            child: Image.network(""),
-          ),
-        )),
-        Expanded(
-            child: Container(
-          height: 100,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Column(children: [
-                Text(
-                  "Product Name",
-                  style:
-                      TextStyle(color: textColor, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  "1000\/=/100 grams",
-                  style: TextStyle(
-                    color: Colors.grey,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        children: [
+          Expanded(
+              child: Container(
+            height: 100,
+            child: Center(
+              child: Image.network(
+                  "https://www.veggycation.com.au/siteassets/veggycationvegetable/basil.jpg"),
+            ),
+          )),
+          Expanded(
+              child: Container(
+            height: 100,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(children: [
+                  Text(
+                    "Product Name",
+                    style: TextStyle(
+                        color: textColor, fontWeight: FontWeight.bold),
                   ),
-                ),
-              ]),
-            ],
-          ),
-        )),
-        Expanded(
+                  Text(
+                    "1000\/=/100 grams",
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ]),
+                Container(
+                  margin: EdgeInsets.only(right: 15),
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  height: 35,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          "100 grams",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                      Center(
+                        child: Icon(
+                          Icons.arrow_drop_down,
+                          size: 20,
+                          color: primarycolor,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )),
+          Expanded(
+              child: Container(
+            height: 100,
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 32),
             child: Container(
-          height: 100,
-        ))
-      ],
+              height: 25,
+              width: 50,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.add,
+                      color: primarycolor,
+                      size: 20,
+                    ),
+                    Text(
+                      "ADD",
+                      style: TextStyle(
+                        color: primarycolor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ))
+        ],
+      ),
     );
   }
 }
